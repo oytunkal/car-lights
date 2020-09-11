7c05ae23e45eb9489ffba0b23c1b5115523ec213
Oytun Kal <9095700+oytunkal@users.noreply.github.com>
2020-09-11 12:04:33 +0300

    Update gesture-handler.js

--- gesture-handler.js
+++ gesture-handler.js
@@ -46,9 +46,9 @@ AFRAME.registerComponent('gesture-handler', {
 
     handlePosition: function(event) {
         if (this.isVisible) {
-            this.el.object3D.rotation.y +=
+            this.el.object3D.position.y +=
                 event.detail.positionChange.x * this.data.positionFactor;
-            this.el.object3D.rotation.x +=
+            this.el.object3D.position.x +=
                 event.detail.positionChange.y * this.data.positionFactor;
         }
     },
